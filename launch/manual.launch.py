0c1d46c4b7146f8e3c64dba258a2fd9ec38e356d
1moule <1961466188@qq.com>
2025-02-13 20:17:09 +0800

    Get params.

--- launch/manual.launch.py
+++ launch/manual.launch.py
@@ -1,12 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory("rm_ros2_manual"),
+        'cfg',
+        'manual.yaml'
+    )
     return LaunchDescription([
         Node(
             package='rm_ros2_manual',
             executable='rm_ros2_manual',
-            name='rm_ros2_manual'
+            name='rm_ros2_manual',
+            parameters=[config]
         )
     ])
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("rm_manual");
+  auto node = rclcpp::Node::make_shared("rm_ros2_manual");
   auto node_referee = node->create_sub_node("rm_referee");
 
   std::string robot;
@@ -16,24 +16,13 @@ int main(int argc, char** argv)
 
   std::shared_ptr<rm_ros2_manual::ManualBase> manual_control;
 
-  // if (robot == "standard")
-  //   manual_control = std::make_shared<rm_ros2_manual::ChassisGimbalShooterCoverManual>(nh, nh_referee);
-  // else if ((robot == "hero") || (robot == "drone"))
-  //   manual_control = std::make_shared<rm_ros2_manual::ChassisGimbalShooterManual>(nh, nh_referee);
-  // else if (robot == "engineer")
-  //   manual_control = std::make_shared<rm_ros2_manual::EngineerManual>(nh, nh_referee);
-  // else if (robot == "engineer2")
-  //   manual_control = std::make_shared<rm_ros2_manual::Engineer2Manual>(nh, nh_referee);
-  // else if (robot == "dart")
-  //   manual_control = std::make_shared<rm_ros2_manual::DartManual>(nh, nh_referee);
-  // else if (robot == "balance")
-  //   manual_control = std::make_shared<rm_ros2_manual::BalanceManual>(nh, nh_referee);
-  // else
-  // {
-  //   RCLCPP_ERROR(nh->get_logger(), "No robot type specified");
-  //   return 0;
-  // }
-  manual_control = std::make_shared<rm_ros2_manual::StandardManual>(node);
+  if (robot == "standard")
+    manual_control = std::make_shared<rm_ros2_manual::StandardManual>(node);
+  else
+  {
+    RCLCPP_ERROR(node->get_logger(), "No robot type specified");
+    return 0;
+  }
 
   rclcpp::Rate loop_rate(100);
   while (rclcpp::ok())
