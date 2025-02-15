3a9b462368a2049114b261f7521619e24d7a1657
1moule <1961466188@qq.com>
2025-02-15 12:16:18 +0800

    Update test launch and cfg.

--- launch/manual.launch.py
+++ launch/manual.launch.py
@@ -3,17 +3,25 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
-    config = os.path.join(
+    manual_config = os.path.join(
         get_package_share_directory("rm_ros2_manual"),
         'cfg',
         'manual.yaml'
+    )
+    controller_config = os.path.join(
+        get_package_share_directory("rm_description"),
+        'cfg',
+        'test.yaml'
     )
     return LaunchDescription([
         Node(
             package='rm_ros2_manual',
             executable='rm_ros2_manual',
             name='rm_ros2_manual',
-            parameters=[config]
+            parameters=[manual_config, controller_config],
+            output='screen',
+            prefix=['gdb -ex run --args'],
         )
     ])
